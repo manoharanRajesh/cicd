dbe153cdcd0e143c5fbfc952cd7b858c003db97f
Rajesh mano <mano.rajesh@gmail.com>
2018-05-01 00:12:25 +0200

    update lib

--- jenkins/lib/cf.groovy
+++ jenkins/lib/cf.groovy
@@ -1,7 +1,11 @@
 
-dev push(space,org,url){
+def push(space,org,url){
     pushToCloudFoundry cloudSpace: space, credentialsId: 'cfId', organization: org, target: url
 
+}
+
+def deployApp(){
+    pushToCloudFoundry cloudSpace: 't-space', credentialsId: 'pcf', organization: 'g-org', target: 'https://api.run.pivotal.io'
 }
 
 return this;
