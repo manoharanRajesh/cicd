d8969547483503b1cadba4e274e6d00c2983992e
manoharanRajesh <mano.rajesh@gmail.com>
2018-08-02 21:16:14 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo apt-get install git
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/manoharanRajesh/cicd.git && cd cicd && git checkout dev && cd ./jenkins/runnerv1 && docker-compose up
+git clone https://github.com/manoharanRajesh/cicd.git && cd cicd && git checkout dev && cd ./jenkins/runner && docker-compose up
