8844433b62540cea99cb3d839fd08bfa03d247c9
Rajesh mano <mano.rajesh@gmail.com>
2018-08-23 01:06:42 +0200

    slave restart script.

---
+++ restart-8081.sh
@@ -0,0 +1,1 @@
+cd /cicd/jenkins/runner && docker-compose stop slave1 && docker-compose start slave1
---
+++ restart-8082.sh
@@ -0,0 +1,1 @@
+cd /cicd/jenkins/runner && docker-compose stop slave && docker-compose start slave && docker-compose stop slave2 && docker-compose start slave2
